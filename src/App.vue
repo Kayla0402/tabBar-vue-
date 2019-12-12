0dae2d4e0e16ccda80d4dd87cd599526db534652
Kayla0402 <912454063@qq.com>
2019-12-12 23:49:29 +0800

    tabBar（vue)

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from './components/MainTabBar'
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+  .tab_bar_item img{
+    width: 24px;
+    height: 24px;
+    background-color: bisque;
+    vertical-align: middle;
+  }
+  .active{
+    color:red
+  }
+</style>
---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img slot="item_icon" src="../assets/image/tabBar/1.png" alt="">
+      <img slot="item_icon_active" src="../assets/image/tabBar/1_1.png" alt="">
+      <div slot="item_title">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="yellow">
+      <img slot="item_icon" src="~assets/image/tabBar/1.png" alt="">
+      <img slot="item_icon_active" src="~assets/image/tabBar/1_1.png" alt="">
+      <div slot="item_title">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="red">
+      <img slot="item_icon" src="../assets/image/tabBar/1.png" alt="">
+      <img slot="item_icon_active" src="../assets/image/tabBar/1_1.png" alt="">
+      <div slot="item_title">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="green">
+      <img slot="item_icon" src="../assets/image/tabBar/1.png" alt="">
+      <img slot="item_icon_active" src="../assets/image/tabBar/1_1.png" alt="">
+      <div slot="item_title">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import tabBar from 'components/tabBar/TarBar'
+  import tabBarItem from 'components/tabBar/TabBarItem'
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      tabBar,
+      tabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
